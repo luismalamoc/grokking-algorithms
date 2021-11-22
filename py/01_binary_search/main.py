76b18e2bb0e5f1481eaa6f0ff6d5a910d12fbd15
Luis Mauricio Alamo <luismalamoc@gmail.com>
2021-11-22 11:53:29 -0300

    feat: python binary search algorithm was added

---
+++ py/01_binary_search/main.py
@@ -0,0 +1,24 @@
+def binay_search(list, item):
+    low = 0
+    high = len(list) -1
+
+    while low <= high:
+        mid = int((low + high) / 2)
+        guess = list[mid]
+        
+        if guess == item:
+            return mid
+        
+        if guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+        
+    return None
+
+my_list = [1, 3, 5, 7, 9]
+
+assert binay_search(my_list, 3) == 1
+assert binay_search(my_list, -1) == None
+print(binay_search(my_list, 3))
+print(binay_search(my_list, -1))
